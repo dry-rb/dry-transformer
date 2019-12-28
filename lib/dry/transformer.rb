4b02fe5a6a7f90dd63b979e5cf1bf9d9e1d720f9
Piotr Solnica <piotr.solnica@gmail.com>
2019-12-28 12:14:13 +0100

    Remove old parts originally copied from transproc

--- lib/dry/transformer.rb
+++ lib/dry/transformer.rb
@@ -3,12 +3,9 @@
 require 'dry/transformer/version'
 require 'dry/transformer/constants'
 require 'dry/transformer/function'
-require 'dry/transformer/functions'
-require 'dry/transformer/composer'
 require 'dry/transformer/error'
 require 'dry/transformer/store'
 require 'dry/transformer/registry'
-require 'dry/transformer/support/deprecations'
 
 require 'dry/transformer/array'
 require 'dry/transformer/hash'
