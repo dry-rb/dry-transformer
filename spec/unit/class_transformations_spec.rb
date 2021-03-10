df1998828dd9d6faaace586b60b46f6c585adda8
Pat Allan <pat@freelancing-gods.com>
2021-03-10 13:13:51 +1100

    Update reference to dry/equalizer.
    I presume it’s something that was refactored out of dry-core?

--- spec/unit/class_transformations_spec.rb
+++ spec/unit/class_transformations_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "dry/core/equalizer"
+require "dry/equalizer"
 
 RSpec.describe Dry::Transformer::ClassTransformations do
   describe ".constructor_inject" do
--- spec/unit/transformer/class_interface_spec.rb
+++ spec/unit/transformer/class_interface_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'ostruct'
-require 'dry/core/equalizer'
+require 'dry/equalizer'
 
 RSpec.describe Dry::Transformer do
   let(:container) { Module.new { extend Dry::Transformer::Registry } }
